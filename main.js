cfa187cc14ec4e4c672ea50df5cd5c05b450a7e9
iagosm <iagosousa201486@gmail.com>
2024-03-10 00:00:14 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+  if (elemento !== null && elemento.localName === 'audio') {
+    elemento.play();
+  }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++)  {
+  const tecla = listaDeTeclas[contador];
+  let instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; // template string
+  tecla.onclick = function() {
+    tocaSom(idAudio);
+  }
+  
+  tecla.onkeydown = function (evento) {
+    if(evento.code === "Enter" || evento.code === "Space") {
+    tecla.classList.add('ativa');
+    }
+  }
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+  contador++;
+}
